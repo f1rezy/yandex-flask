98bdd9d1882e6af55440454c14e8f6c599665859
Matvey <atallab@ya.ru>
2023-04-17 19:47:15 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask_restful import Api
+
+import items_resources
 from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+api = Api(app)
 
 
 def main():
     db_session.global_init("db/database.db")
+    api.add_resource(items_resources.ItemResource, '/api/items/<int:item_id>')
+    api.add_resource(items_resources.ItemsListResource, '/api/items')
+    api.add_resource(items_resources.ItemsCategoryResource, '/api/items/category/<int:category_id>')
     app.run()
 
 
