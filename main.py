12b7fe18ea2295e8ce742dc35897d66eb4262bb3
Daniil Litvinenko <danil.fire@bk.ru>
2023-04-17 23:18:23 +0700

    Create main page

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
+from data.users import User
+from data.items import Items
+from data.categories import Categories
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -8,6 +11,13 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/database.db")
     app.run()
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    items = db_sess.query(Items).all()
+    return render_template("index.html", title="Главная", items=items)
 
 
 if __name__ == '__main__':
